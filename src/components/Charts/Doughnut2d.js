d49a24f0a15accb2fe507aebcabbe0e34c22aa09
Man Khi Kim <47694895+Fomachka@users.noreply.github.com>
2022-01-11 13:59:25 +0900

    Update Doughnut2d.js

--- src/components/Charts/Doughnut2d.js
+++ src/components/Charts/Doughnut2d.js
@@ -1,35 +1,18 @@
-// STEP 1 - Include Dependencies
-// Include react
 import React from "react";
-
-// Include the react-fusioncharts component
 import ReactFC from "react-fusioncharts";
-
-// Include the fusioncharts library
 import FusionCharts from "fusioncharts";
-
-// Include the chart type
 import Chart from "fusioncharts/fusioncharts.charts";
-
-// Include the theme as fusion
 import FusionTheme from "fusioncharts/themes/fusioncharts.theme.candy";
 
-// Adding the chart and theme as dependency to the core fusioncharts
 ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
 
-// STEP 2 - Chart Data
-
-// STEP 3 - Creating the JSON object to store the chart configurations
-
-// STEP 4 - Creating the DOM element to pass the react-fusioncharts component
 const Doughtnut2d = ({ data }) => {
   const chartConfigs = {
-    type: "doughnut2d", // The chart type
-    width: "100%", // Width of the chart
-    height: "400", // Height of the chart
-    dataFormat: "json", // Data type
+    type: "doughnut2d", 
+    width: "100%", 
+    height: "400", 
+    dataFormat: "json", 
     dataSource: {
-      // Chart Configuration
       chart: {
         caption: "Stars Per Language",
         theme: "fusion",
@@ -39,7 +22,6 @@ const Doughtnut2d = ({ data }) => {
         theme: "candy",
         paletteColors: "#C9AB89, #efe6dc, #504437,#3c3329,#f4eee7",
       },
-      // Chart Data
       data: data,
     },
   };
