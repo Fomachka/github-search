3ddd5354bd0c7482fb354f66c21c48429edaf344
Man Khi Kim <47694895+Fomachka@users.noreply.github.com>
2022-01-11 13:58:09 +0900

    Update Bar2D.js

--- src/components/Charts/Bar2D.js
+++ src/components/Charts/Bar2D.js
@@ -1,35 +1,19 @@
-// STEP 1 - Include Dependencies
-// Include react
 import React from "react";
-
-// Include the react-fusioncharts component
 import ReactFC from "react-fusioncharts";
-
-// Include the fusioncharts library
 import FusionCharts from "fusioncharts";
-
-// Include the chart type
 import Chart from "fusioncharts/fusioncharts.charts";
-
-// Include the theme as fusion
 import FusionTheme from "fusioncharts/themes/fusioncharts.theme.candy";
 
-// Adding the chart and theme as dependency to the core fusioncharts
 ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
 
-// STEP 2 - Chart Data
 
-// STEP 3 - Creating the JSON object to store the chart configurations
-
-// STEP 4 - Creating the DOM element to pass the react-fusioncharts component
 const Bar2d = ({ data }) => {
   const chartConfigs = {
-    type: "bar2d", // The chart type
-    width: "100%", // Width of the chart
-    height: "400", // Height of the chart
-    dataFormat: "json", // Data type
+    type: "bar2d", 
+    width: "100%",
+    height: "400", 
+    dataFormat: "json", 
     dataSource: {
-      // Chart Configuration
       chart: {
         caption: "Most Forked",
         yAxis: "Forks",
@@ -39,7 +23,6 @@ const Bar2d = ({ data }) => {
         yAxisNameFontSize: "16px",
         paletteColors: "#C9AB89, #efe6dc, #504437,#3c3329,#f4eee7",
       },
-      // Chart Data
       data: data,
     },
   };
